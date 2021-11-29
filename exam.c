1866e7c2912c74e3d81d22d11ea0b6a9506fb8a0
João Pedro Miranda Marques <44896891+jueta@users.noreply.github.com>
2021-11-29 17:25:04 -0300

    Add files via upload

--- exam.c
+++ exam.c
@@ -6,43 +6,56 @@
 //----------------------------------------------------------
 #include <stdio.h>
 
-int main(){
-
+typedef struct{
     int id; //Identificador
     int sq; //Sequenciador
     int vl; //Valor
-    int size = sizeof(id) + sizeof(sq) + sizeof(vl) + 1;
+}Data;
 
+
+void serialize(Data *data, unsigned char* value[]){
+
+    int size = sizeof(data->id) + sizeof(data->sq) + sizeof(data->vl) + 1;
+
+    value[0] = (unsigned char*)&size;
+    value[1] = (unsigned char*)&data->id;
+    value[2] = (unsigned char*)&data->sq;
+    value[3] = (unsigned char*)&data->vl;
+
+}
+
+int main(){
+
+    Data data;
     //Teste    <- Insira aqui os valores que querem testar.
-    id = 0x03;
-    sq = 0x05;
-    vl = 0xFFFF;
+    data.id = 0x03;
+    data.sq = 0x05;
+    data.vl = 0xFFFF;
 
-    unsigned char* value0 = (unsigned char*)&size;
-    unsigned char* value1 = (unsigned char*)&id;
-    unsigned char* value2 = (unsigned char*)&sq;
-    unsigned char* value3 = (unsigned char*)&vl;
+    unsigned char* value[4];
+
+    serialize(&data, &value);
 
     // size
-    printf("%02x-",  value0[0]); 
+    printf("%02x-",  value[0][0]); 
 
     // Identificador
-    printf("%02x-", value1[0]); 
-    printf("%02x-", value1[1]); 
-    printf("%02x-", value1[2]); 
-    printf("%02x-", value1[3]); 
+    printf("%02x-", value[1][0]); 
+    printf("%02x-", value[1][1]); 
+    printf("%02x-", value[1][2]); 
+    printf("%02x-", value[1][3]); 
 
     //Sequenciador
-    printf("%02x-", value2[0]); 
-    printf("%02x-", value2[1]); 
-    printf("%02x-", value2[2]); 
-    printf("%02x-", value2[3]); 
+    printf("%02x-", value[2][0]); 
+    printf("%02x-", value[2][1]); 
+    printf("%02x-", value[2][2]); 
+    printf("%02x-", value[2][3]); 
 
     //Valor
-    printf("%02x-", value3[0]); 
-    printf("%02x-", value3[1]); 
-    printf("%02x-", value3[2]); 
-    printf("%02x\n", value3[3]);
+    printf("%02x-", value[3][0]); 
+    printf("%02x-", value[3][1]); 
+    printf("%02x-", value[3][2]); 
+    printf("%02x\n", value[3][3]);
 
     printf("Little Endian\n"); 
 
